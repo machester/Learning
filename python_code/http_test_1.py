a616bbbd44e1958622bc936e2930f7c9be5e1df1
machester <shusheng1991@gmail.com>
2020-05-10 17:05:36 +0800

    add android virtural touch panel driver, no need to config dts

---
+++ python_code/http_test_1.py
@@ -0,0 +1,11 @@
+from http.client import HTTPConnection
+from urllib import parse
+
+c = HTTPConnection("www.python.org", 80)
+c.request("HEAD", "/index/html")
+
+resp = c.getresponse()
+
+print("status", resp.status)
+for name, value in resp.getheaders():
+        print(name, value)
