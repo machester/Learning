1292a37f9592cebd6555bd645b33d2dd418350f9
machester <shusheng1991@gmail.com>
2020-11-13 10:00:22 +0800

    update python code 2020-11-13

---
+++ python_code/python_function/Decorator/metric.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+import sys
+
+def metric(fn):
+    print('%s executed in %s ms' % (fn.__name__, 10.24))
+    return fn
+
+
+ @metric
+def fast(x, y):
+    time.sleep(0.0012)
+    return x + y;
+
+@metric
+def slow(x, y, z):
+    time.sleep(0.1234)
+    return x * y * z;
+
+f = fast(11, 22)
+s = slow(11, 22, 33)
+if f != 33:
+    print('测试失败!')
+elif s != 7986:
+    print('测试失败!')
