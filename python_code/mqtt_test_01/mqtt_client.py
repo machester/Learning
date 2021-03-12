b2e22640dfce195be2b768474b0a6f5da6198fa4
machester <shusheng1991@gmail.com>
2021-03-12 18:06:43 +0800

    python mqtt client try json format data

--- python_code/mqtt_test_01/mqtt_client.py
+++ python_code/mqtt_test_01/mqtt_client.py
@@ -1,24 +1,35 @@
+# *---- encoding uft-8 ------------*
+import os
 import time
 import threading
 import inspect
 import ctypes
 import random
 import pymysql
+import json
+import demjson
 import paho.mqtt.client as mqtt
 
-MQTT_SERVER_IP="59.47.233.179"
-MQTT_SERVER_TOPIC="temp"
+MQTT_SERVER_IP = "59.47.233.179"
+MQTT_SERVER_TOPIC = "temp"
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code: " + str(rc))
+    print("waitting for message:")
+
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
+    # demjson.encode(msg)
 
-client = mqtt.Client()
+client_id = f'python-mqtt-{random.randint(0, 100)}'
+print("client_id: " + client_id)
+client = mqtt.Client(client_id)
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect(MQTT_SERVER_IP, 1883, 600) # 600为keepalive的时间间隔
+client.connect(MQTT_SERVER_IP, 1883, 60)  # 60s keepalive
+# qos=0
 client.subscribe(MQTT_SERVER_TOPIC, qos=0)
-print("waitting for message:")
-client.loop_forever() # 保持连接, qos=0)
+# keep connect
+client.loop_forever()
